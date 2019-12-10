527aa7221ef5f348f08338cb7b6750a658ca0fb6
helllth <th@cismet.de>
2019-12-10 15:42:10 +0100

    changed bin name and tmp disabled the import command

--- src/cs-conf.js
+++ src/cs-conf.js
@@ -4,68 +4,59 @@ import program from 'commander';
 import * as csExport from './lib/export';
 import * as csImport from './lib/import';
 
-program
-    .version('0.1.0')
-    .option('-c, --config <path>', 'set config path. ', './runtime.properties')
+program.version('0.9.0').option('-c, --config <path>', 'set config path. ', './runtime.properties');
 
-
-program
-    .command('import')
-    .alias('i')
-    .description('imports the meta information into the cids system')
-    .option('-f, --folder <folder>', 'the folder where the config is', "config")
-    .option('-s, --schema <schema>', 'the schema where the cs-Tables will be', "public")
-    .option('-o, --only', 'Only import the following topics')
-    .option('-x, --skip', 'Skip the import of the following topics')
-    .option('-C, --classes', 'The classes with their attributes and permissions')
-    .option('-S, --structure', 'The structure information of the system')
-    .option('-U, --usermanagement', 'The users and their groups')
-    .action(function (cmd) {
-        console.log("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< IMPORT")
-        console.log("... from " + cmd.folder);
-        console.log("... to " + cmd.schema + ".cs_*");
-        console.log("... for " + cmd.parent.config );
-        csImport.worker(cmd.folder, cmd.schema, cmd.parent.config);
-
-    });
+// program
+// 	.command('import')
+// 	.alias('i')
+// 	.description('imports the meta information into the cids system')
+// 	.option('-f, --folder <folder>', 'the folder where the config is', 'config')
+// 	.option('-s, --schema <schema>', 'the schema where the cs-Tables will be', 'public')
+// 	.option('-o, --only', 'Only import the following topics')
+// 	.option('-x, --skip', 'Skip the import of the following topics')
+// 	.option('-C, --classes', 'The classes with their attributes and permissions')
+// 	.option('-S, --structure', 'The structure information of the system')
+// 	.option('-U, --usermanagement', 'The users and their groups')
+// 	.action(function(cmd) {
+// 		console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< IMPORT');
+// 		console.log('... from ' + cmd.folder);
+// 		console.log('... to ' + cmd.schema + '.cs_*');
+// 		console.log('... for ' + cmd.parent.config);
+// 		csImport.worker(cmd.folder, cmd.schema, cmd.parent.config);
+// 	});
 
 program
-    .command('export')
-    .alias('e')
-    .description('exports the meta information of a cids system')
-    .option('-f, --folder <folder>', 'the folder where the config will be written', "config")
-    .option('-s, --schema <schema>', 'the schema where the cs-Tables are', "public")
-    .option('-o, --only', 'Only export the following topics')
-    .option('-x, --skip', 'Skip the export of the following topics')
-    .option('-C, --classes', 'The classes with their attributes and permissions')
-    .option('-S, --structure', 'The structure information of the system')
-    .option('-U, --usermanagement', 'The users and their groups')
-    .action(function (cmd) {
-        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EXPORT")
-        console.log("... to " + cmd.folder);
-        console.log("... from " + cmd.schema + ".cs_*");
-        console.log("... for " + cmd.parent.config );
-        csExport.worker(cmd.folder, cmd.schema, cmd.parent.config);
-    });
-
-
+	.command('export')
+	.alias('e')
+	.description('exports the meta information of a cids system')
+	.option('-f, --folder <folder>', 'the folder where the config will be written', 'config')
+	.option('-s, --schema <schema>', 'the schema where the cs-Tables are', 'public')
+	.option('-o, --only', 'Only export the following topics')
+	.option('-x, --skip', 'Skip the export of the following topics')
+	.option('-C, --classes', 'The classes with their attributes and permissions')
+	.option('-S, --structure', 'The structure information of the system')
+	.option('-U, --usermanagement', 'The users and their groups')
+	.action(function(cmd) {
+		console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> EXPORT');
+		console.log('... to ' + cmd.folder);
+		console.log('... from ' + cmd.schema + '.cs_*');
+		console.log('... for ' + cmd.parent.config);
+		csExport.worker(cmd.folder, cmd.schema, cmd.parent.config);
+	});
 
 // Parse the commandline
-//program.parse(process.argv);
-
-
+program.parse(process.argv);
 
 // For development purpose
 // Should be commented out
 
 // // EXPORT
-program.parse(['node',
-    'dev-cs-conf',           
-    "-c", "./runtime/runtime.properties",
-    'e',
-    '-f', 'export'
-]);
-
+// program.parse(['node',
+//     'dev-cs-conf',
+//     "-c", "./runtime/runtime.properties",
+//     'e',
+//     '-f', 'export'
+// ]);
 
 // // IMPORT
 // program.parse(['node',
@@ -75,7 +66,6 @@ program.parse(['node',
 //     '-f', 'export',
 //     '-s', '_demo',
 // ]);
-
 
 // Helptexts
 // program.parse(['node',
