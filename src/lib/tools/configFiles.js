8243a516dd3e1e1bfc595d87084f747ee87abf34
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2022-07-02 00:07:05 +0200

    check if exists before rm

--- src/lib/tools/configFiles.js
+++ src/lib/tools/configFiles.js
@@ -129,47 +129,47 @@ export function writeConfigFiles(config, configDir, overwrite = false) {
 
     if (domains != null) {
         fs.writeFileSync(util.format("%s/domains.json", configDir), stringify(domains), "utf8");
-    } else {
+    } else if (fs.existsSync(util.format("%s/domains.json", configDir))) {
         fs.rmSync(util.format("%s/domains.json", configDir));
     }
     if (policyRules != null) {
         fs.writeFileSync(util.format("%s/policyRules.json", configDir), stringify(policyRules), "utf8");
-    } else {
+    } else if (fs.existsSync(util.format("%s/policyRules.json", configDir))) {
         fs.rmSync(util.format("%s/policyRules.json", configDir));
     }
     if (usergroups != null) {
         fs.writeFileSync(util.format("%s/usergroups.json", configDir), stringify(usergroups, { maxLength: 160 }), "utf8");
-    } else {
+    } else if (fs.existsSync(util.format("%s/usergroups.json", configDir))) {
         fs.rmSync(util.format("%s/usergroups.json", configDir));
     }
     if (usermanagement != null) {
         fs.writeFileSync(util.format("%s/usermanagement.json", configDir), stringify(usermanagement, { maxLength: 120 }), "utf8");
-    } else {
+    } else if (fs.existsSync(util.format("%s/usermanagement.json", configDir))) {
         fs.rmSync(util.format("%s/usermanagement.json", configDir));
     }
     if (classes != null) {
         fs.writeFileSync(util.format("%s/classes.json", configDir), stringify(classes, { maxLength: 100 }), "utf8");
-    } else {
+    } else if (fs.existsSync(util.format("%s/classes.json", configDir))) {
         fs.rmSync(util.format("%s/classes.json", configDir));
     }
     if (classPerms != null) {
         fs.writeFileSync(util.format("%s/classPerms.json", configDir), stringify(classPerms, { maxLength: 100 }), "utf8");
-    } else {
+    } else if (fs.existsSync(util.format("%s/classPerms.json", configDir))) {
         fs.rmSync(util.format("%s/classPerms.json", configDir));
     }
     if (attrPerms != null) {
         fs.writeFileSync(util.format("%s/attrPerms.json", configDir), stringify(attrPerms, { maxLength: 100 }), "utf8");
-    } else {
+    } else if (fs.existsSync(util.format("%s/attrPerms.json", configDir))) {
         fs.rmSync(util.format("%s/attrPerms.json", configDir));
     }
     if (structure != null) {
         fs.writeFileSync(util.format("%s/structure.json", configDir), stringify(structure, { maxLength: 80 }), "utf8");
-    } else {
+    } else if (fs.existsSync(util.format("%s/structure.json", configDir))) {
         fs.rmSync(util.format("%s/structure.json", configDir));
     }
     if (dynchildhelpers != null) {
         fs.writeFileSync(util.format("%s/dynchildhelpers.json", configDir), stringify(dynchildhelpers, { maxLength: 80 }), "utf8");
-    } else {
+    } else if (fs.existsSync(util.format("%s/dynchildhelpers.json", configDir))) {
         fs.rmSync(util.format("%s/dynchildhelpers.json", configDir));
     }
 
