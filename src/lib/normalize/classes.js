f9796eda1f41237c68d4d9f125dc51be24bb5303
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2023-10-04 16:09:04 +0200

    fixing order of attributes of "auto" classes while reorganizing

--- src/lib/normalize/classes.js
+++ src/lib/normalize/classes.js
@@ -2,39 +2,44 @@ import util from 'util';
 import normalizeAttributes from "./attributes";
 import { defaultClass } from "../tools/defaultObjects";
 
-function normalizeClasses(classes) {
+export default function normalizeClasses(classes) {
     let normalized = [];
     
     if (classes != null) {
         for (let clazz of classes) {
-            if (clazz.table == null) throw "normalizeClasses: missing table for class";
-            //if (clazz.table !== clazz.table.toUpperCase()) throw util.format("normalizeClasses: table '%s' has to be uppercase", clazz.table);
-            if (clazz.pk === null) throw util.format("normalizeClasses: [%s] pk of can't be null", clazz.table);
-            //if (clazz.pk !== undefined && clazz.pk !== clazz.pk.toUpperCase()) throw util.format("normalizeClasses: pk '%s' has to be uppercase", clazz.pk);
-            //if (clazz.cidsType !== undefined && clazz.cidsType !== clazz.cidsType.toUpperCase()) throw util.format("normalizeClasses: cidsType '%s' has to be uppercase", clazz.cidsType);
-            //if (clazz.oneToMany !== undefined && clazz.oneToMany !== clazz.oneToMany.toUpperCase()) throw util.format("normalizeClasses: oneToMany '%s' has to be uppercase", clazz.oneToMany);
-            //if (clazz.manyToMany !== undefined && clazz.manyToMany !== clazz.manyToMany.toUpperCase()) throw util.format("normalizeClasses: manyToMany '%s' has to be uppercase", clazz.manyToMany);
-
-            if (clazz.table != null) {
-                clazz.table = clazz.table.toLowerCase();
-            }            
-            if (clazz.pk != null) {
-                clazz.pk = clazz.pk.toLowerCase();
-            }            
-
-            normalized.push(Object.assign({}, defaultClass, clazz, {
-                name: clazz.name != null ? clazz.name : clazz.table,
-                toString: normalizeSpecial(clazz.toString, clazz.table),
-                editor: normalizeSpecial(clazz.editor, clazz.table),
-                renderer: normalizeSpecial(clazz.renderer, clazz.table),
-                attributes: normalizeAttributes(clazz.attributes, clazz.pk, clazz.table),
-                icon: null,
-                classIcon: clazz.classIcon || clazz.icon || null,
-                objectIcon: clazz.objectIcon || clazz.icon || null,
-            }));
+            let normalizedClass = normalizeClass(clazz);
+            normalized.push(normalizedClass);                
         }
     }
     return normalized;
+}
+
+export function normalizeClass(clazz) {
+    if (clazz.table == null) throw "normalizeClasses: missing table for class";
+    //if (clazz.table !== clazz.table.toUpperCase()) throw util.format("normalizeClasses: table '%s' has to be uppercase", clazz.table);
+    if (clazz.pk === null) throw util.format("normalizeClasses: [%s] pk of can't be null", clazz.table);
+    //if (clazz.pk !== undefined && clazz.pk !== clazz.pk.toUpperCase()) throw util.format("normalizeClasses: pk '%s' has to be uppercase", clazz.pk);
+    //if (clazz.cidsType !== undefined && clazz.cidsType !== clazz.cidsType.toUpperCase()) throw util.format("normalizeClasses: cidsType '%s' has to be uppercase", clazz.cidsType);
+    //if (clazz.oneToMany !== undefined && clazz.oneToMany !== clazz.oneToMany.toUpperCase()) throw util.format("normalizeClasses: oneToMany '%s' has to be uppercase", clazz.oneToMany);
+    //if (clazz.manyToMany !== undefined && clazz.manyToMany !== clazz.manyToMany.toUpperCase()) throw util.format("normalizeClasses: manyToMany '%s' has to be uppercase", clazz.manyToMany);
+
+    if (clazz.table != null) {
+        clazz.table = clazz.table.toLowerCase();
+    }            
+    if (clazz.pk != null) {
+        clazz.pk = clazz.pk.toLowerCase();
+    }            
+
+    return Object.assign({}, defaultClass, clazz, {
+        name: clazz.name != null ? clazz.name : clazz.table,
+        toString: normalizeSpecial(clazz.toString, clazz.table),
+        editor: normalizeSpecial(clazz.editor, clazz.table),
+        renderer: normalizeSpecial(clazz.renderer, clazz.table),
+        attributes: normalizeAttributes(clazz.attributes, clazz.pk, clazz.table),
+        icon: null,
+        classIcon: clazz.classIcon || clazz.icon || null,
+        objectIcon: clazz.objectIcon || clazz.icon || null,
+    });
 }
 
 function normalizeSpecial(special, table) {
@@ -49,5 +54,3 @@ function normalizeSpecial(special, table) {
     }
     return null;
 }
-
-export default normalizeClasses;
--- src/lib/reorganize/classes.js
+++ src/lib/reorganize/classes.js
@@ -1,8 +1,9 @@
+import { normalizeClass } from "../normalize/classes";
 
 function reorganizeClasses(classes) {
     if (classes != null) {
         for (let clazz of classes) {
-            if (clazz.attributesOrder == "auto") {
+            if (normalizeClass(clazz).attributesOrder == "auto") {
                 if (clazz.attributes != null) {
                     clazz.attributes = clazz.attributes.sort((a, b) => { 
                         return a.field.localeCompare(b.field);
