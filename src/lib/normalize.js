185f4b8d867bc7ed7f43e1742fe8601dba9fdd45
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2023-11-10 21:33:09 +0100

    fixing default type for structure in normalize

--- src/lib/normalize.js
+++ src/lib/normalize.js
@@ -218,7 +218,7 @@ export function normalizeConfigPolicyRule(policyRule = {}, policyRuleKey) {
     return normalized;
 }
 
-export function normalizeStructure(structure = {}) {
+export function normalizeStructure(structure = []) {
        return normalizeNodes(structure);
 }
 
